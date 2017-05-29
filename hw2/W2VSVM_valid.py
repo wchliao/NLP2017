b995e16ca4340654e2a371f4dc537c9e27670136
chipyaya <b03902024@ntu.edu.tw>
2017-05-29 18:56:11 +0800

    add cross validation version of w2vsvm

--- hw2/W2VSVM_valid.py
+++ hw2/W2VSVM_valid.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from sklearn import svm
 import gensim
+from joblib import Parallel, delayed    #NEED TO INSTALL!!!
+import multiprocessing
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -41,6 +43,37 @@ def generateX(df, vectorSize):
         X[i] = np.concatenate((np.sum(np.asarray(model[term]) for term in df['Clause1'][i].split(' ')), np.sum(np.asarray(model[term]) for term in df['Clause2'][i].split(' '))), axis=0)
     return X
 
+def trainAndValid(i, vectorSize):
+    #print('Fold', i, '....')
+    df_train = pd.read_csv('./myData/train'+str(i)+'.csv').replace('Expansion', 0).replace('Contingency', 1).replace('Comparison', 2).replace('Temporal', 3).drop('Id', 1)
+
+    # Balance train
+    df_train_balanced = balanceData(df_train)
+
+    # Read valid
+    df_valid = pd.read_csv('./myData/valid'+str(i)+'.csv').replace('Expansion', 0).replace('Contingency', 1).replace('Comparison', 2).replace('Temporal', 3).drop('Id', 1)
+
+
+    # Generate X
+    X_train = generateX(df_train_balanced, vectorSize)
+    X_valid = generateX(df_valid, vectorSize)
+    Y_train = list(df_train_balanced['Relation'])
+    Y_valid = list(df_valid['Relation'])
+
+    # Training
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, Y_train)
+
+
+    # Validating
+    acc = 0
+    for ans,predict in zip(Y_valid, lin_clf.predict(X_valid)):
+        if ans == predict:
+             acc += 1
+    acc /= df_valid.shape[0]
+    print('acc:', acc)
+    return acc
+
 if __name__ == '__main__':
 
     args = parse_args()
@@ -49,7 +82,7 @@ if __name__ == '__main__':
     numToRelation = ['Expansion', 'Contingency', 'Comparison', 'Temporal']
 
     # Read complete data
-    df_complete = pd.read_csv('./data/train.simp.seg').replace('Expansion', 0).replace('Contingency', 1).replace('Comparison', 2).replace('Temporal', 3).drop('Id', 1)
+    df_complete = pd.read_csv('./data/train.simp.seg')
 
     # Read test
     df_test = pd.read_csv('./data/test.simp.seg')
@@ -60,21 +93,7 @@ if __name__ == '__main__':
     # Train Word2Vec model
     model = gensim.models.Word2Vec(contents, size=vectorSize, window=5, min_count=0, workers=4)
 
-    df_train_balanced = balanceData(df_complete)
-
-    # Generate X
-    X_train = generateX(df_train_balanced, vectorSize)
-    X_test = generateX(df_test, vectorSize)
-    Y_train = list(df_train_balanced['Relation'])
-
-    # Training
-    lin_clf = svm.LinearSVC()
-    lin_clf.fit(X_train, Y_train)
-
-    # Gernate ans
-    with open('ans'+str(vectorSize)+'.csv', 'w') as f:
-        f.write('Id,Relation\n')
-        for Id, predict in zip(df_test['Id'], lin_clf.predict(X_test)):
-            f.write(str(Id)+","+numToRelation[predict]+'\n')
-    print('Done')
-
+    ### Cross validation
+    accs = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(trainAndValid)(i, vectorSize) for i in range(5))
+    acc_avg = sum(accs) / len(accs)
+    print('acc_avg:', acc_avg)
