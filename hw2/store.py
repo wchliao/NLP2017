3bb9d3cae62013f6463a56e4e21849f480050a89
chipyaya <b03902024@ntu.edu.tw>
2017-06-03 16:48:21 +0800

    debug

--- hw2/store.py
+++ hw2/store.py
@@ -11,9 +11,11 @@ for lines in f:
     if len(terms) == 3:
         if key != "":
             w2v_dict[key] = values
+            values = []
+            values += [float(v.replace('\n','').replace('[','').replace(']','')) for v in terms[2].split(' ') if v.replace('\n','').replace('[','').replace(']','') != '']
         key = terms[1]
     else:
         values += [float(v.replace('\n','').replace('[','').replace(']','')) for v in terms[0].split(' ') if v.replace('\n','').replace('[','').replace(']','') != '']
 
-#pickle.dump( w2v_dict, open( "/tmp2/eee/K/w2v_dict.p", "wb" ) )
+pickle.dump( w2v_dict, open( "/tmp2/eee/K/w2v_dict.p", "wb" ) )
 print('Done')
